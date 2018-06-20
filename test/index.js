6e94ea1a789a03c4eec4f0033c88c7e89a403b0c
Wes Tyler <wtyler@xogrp.com>
2018-06-20 10:26:40 -0500

    Dependency updates; bug fixes with ._flags

--- test/index.js
+++ test/index.js
@@ -11,145 +11,166 @@ const describe = lab.describe;
 const it = lab.it;
 const expect = Code.expect;
 
-describe('enums', () => {
+describe('extensions', () => {
 
-    it('validates an un-mapped integer', (done) => {
+    describe('enums', () => {
 
-        const schema = Joi.number().integer().map({
-            a: 1,
-            b: 2,
-            c: 3
+        it('validates an un-mapped integer', async () => {
+
+            const schema = Joi.number().integer().map({
+                a: 1,
+                b: 2,
+                c: 3
+            });
+
+            const value = await schema.validate(5);
+
+            expect(value).to.equal(5);
         });
-        schema.validate(5, (err, value) => {
 
-            expect(err).to.be.null();
+        it('validates with non-integer numbers', async () => {
 
-            done();
+            const schema = Joi.number().map({
+                a: 1,
+                b: 2,
+                c: 3
+            });
+
+            const value = await schema.validate(3.14);
+
+            expect(value).to.equal(3.14);
         });
-    });
 
-    it('validates with non-integer numbers', (done) => {
+        it('validates an enumerated string', async () => {
 
-        const schema = Joi.number().map({
-            a: 1,
-            b: 2,
-            c: 3
-        });
-        schema.validate(3.14, (err, value) => {
+            const schema = Joi.number().integer().map({
+                a: 1,
+                b: 2,
+                c: 3
+            });
 
-            expect(err).to.be.null();
+            const value = await schema.validate('a');
 
-            done();
-        });
-    });
-
-    it('validates an enumerated string', (done) => {
-
-        const schema = Joi.number().integer().map({
-            a: 1,
-            b: 2,
-            c: 3
-        });
-        schema.validate('a', (err, value) => {
-
-            expect(err).to.be.null();
             expect(value).to.equal(1);
-
-            done();
         });
-    });
 
-    it('fails on a non-enumerated string', (done) => {
+        it('validates when "required"', async () => {
 
-        const schema = Joi.number().integer().map({
-            a: 1,
-            b: 2,
-            c: 3
+            const schema = Joi.number().integer().map({
+                a: 1,
+                b: 2,
+                c: 3
+            }).required();
+
+            const value = await schema.validate('a');
+
+            expect(value).to.equal(1);
         });
-        schema.validate('d', (err, value) => {
 
-            expect(err).to.not.be.null();
+        it('does not interfere with non-map number schema coercion', async () => {
+
+            const schema = Joi.number();
+
+            const value = await schema.validate('3');
+
+            expect(value).to.equal(3);
+
+            const err = await expect(schema.validate('nope')).to.reject();
+
+            expect(err.message).to.equal('"value" must be a number');
+        });
+
+        it('fails on a non-enumerated string', async () => {
+
+            const schema = Joi.number().integer().map({
+                a: 1,
+                b: 2,
+                c: 3
+            });
+
+            const err = await expect(schema.validate('d')).to.reject();
+
             expect(err.message).to.equal('"value" must be a number or one of [a, b, c]');
-
-            done();
         });
-    });
 
-    it('fails without convert', (done) => {
+        it('fails without convert', async () => {
 
-        const schema = Joi.number().integer().map({
-            a: 1,
-            b: 2,
-            c: 3
-        });
-        schema.validate('a', { convert: false }, (err, value) => {
+            const schema = Joi.number().integer().map({
+                a: 1,
+                b: 2,
+                c: 3
+            });
 
-            expect(err).to.not.be.null();
+            const err = await expect(schema.validate('a', { convert: false })).to.reject();
+
             expect(err.message).to.equal('"value" must be a number');
-
-            done();
         });
-    });
 
-    it('fails on missing value', (done) => {
+        it('fails on missing value', async () => {
 
-        const schema = Joi.number().integer().map({
-            a: 1,
-            b: 2,
-            c: 3
-        });
-        schema.validate(null, (err, value) => {
+            const schema = Joi.number().integer().map({
+                a: 1,
+                b: 2,
+                c: 3
+            });
 
-            expect(err).to.not.be.null();
+            const err = await expect(schema.validate(null)).to.reject();
+
             expect(err.message).to.equal('"value" must be a number');
-
-            done();
         });
-    });
 
-    it('fails on non-number and non-string value', (done) => {
+        it('fails on non-number and non-string value', async () => {
 
-        const schema = Joi.number().integer().map({
-            a: 1,
-            b: 2,
-            c: 3
-        });
-        schema.validate(true, (err, value) => {
+            const schema = Joi.number().integer().map({
+                a: 1,
+                b: 2,
+                c: 3
+            });
 
-            expect(err).to.not.be.null();
+            const err = await expect(schema.validate(true)).to.reject();
+
             expect(err.message).to.equal('"value" must be a number');
-
-            done();
         });
-    });
 
-    it('should be described correctly', (done) => {
+        it('should be described correctly', () => {
 
-        const schema = Joi.number().integer().map({
-            a: 1,
-            b: 2,
-            c: 3
-        });
-        expect(schema.describe()).to.equal({
-            type: 'number',
-            invalids: [Infinity, -Infinity],
-            rules: [
-                {
-                    name: 'integer'
-                },
-                {
-                    name: 'map',
-                    arg: {
-                        map: {
-                            a: 1,
-                            b: 2,
-                            c: 3
+            const map = {
+                a: 1,
+                b: 2,
+                c: 3
+            };
+            const schema = Joi.number().integer().map(map);
+
+            expect(schema.describe()).to.equal({
+                type: 'number',
+                invalids: [Infinity, -Infinity],
+                options: {
+                    language: {
+                        number: {
+                            map: 'must be a number or one of {{enums}}'
                         }
+                    }
+                },
+                rules: [
+                    {
+                        name: 'integer'
                     },
-                    description: 'Should map enumerated strings'
+                    {
+                        name: 'map',
+                        arg: {
+                            map: {
+                                a: 1,
+                                b: 2,
+                                c: 3
+                            }
+                        },
+                        description: 'Should map enumerated strings'
+                    }
+                ],
+                flags: {
+                    _map: map
                 }
-            ]
+            });
         });
-        done();
     });
 });
